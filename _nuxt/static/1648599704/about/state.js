4a6eda6e30fe305826eb7300baff9a5b7b7c63dd
unknown <plurp411@gmail.com>
2022-03-29 17:23:32 -0700

    final

---
+++ _nuxt/static/1648599704/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog-template\u002F_nuxt\u002Fstatic\u002F1648599704",layout:"default",error:a,state:{likes:{likes:a}},serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fblog-template\u002F",assetsPath:"\u002Fblog-template\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f7d4e5b9"}}}}(null));
---
+++ _nuxt/static/1648599704/blog/what-we-know/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog-template\u002F_nuxt\u002Fstatic\u002F1648599704",layout:"default",error:a,state:{likes:{likes:a}},serverRendered:true,routePath:"\u002Fblog\u002Fwhat-we-know",config:{_app:{basePath:"\u002Fblog-template\u002F",assetsPath:"\u002Fblog-template\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f7d4e5b9"}}}}(null));
